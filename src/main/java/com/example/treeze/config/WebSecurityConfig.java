3fc2711482e63bfb342ead5824a7e1ea959aa5e5
chanjinYang <ystepanie@naver.com>
2024-05-11 22:58:32 +0900

    modify config

--- src/main/java/com/example/treeze/config/WebSecurityConfig.java
+++ src/main/java/com/example/treeze/config/WebSecurityConfig.java
@@ -16,6 +16,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/signup", "/login").permitAll()
                         .anyRequest().authenticated())
