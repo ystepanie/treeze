60d2ff7d40878f29f4cb499b42a8c4f4429735a4
chanjinYang <ystepanie@naver.com>
2024-05-11 23:29:01 +0900

    test git

--- src/main/java/com/example/treeze/config/WebSecurityConfig.java
+++ src/main/java/com/example/treeze/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) //test git
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/signup", "/login").permitAll()
