84c24150003c9b549af6382551f1afdf91ff45cc
ystepanie <ystepanie@naver.com>
2024-06-14 16:28:39 +0900

    change openai file name

---
+++ src/main/java/com/example/treeze/task/openai/ChangeService.java
@@ -0,0 +1,15 @@
+package com.example.treeze.task.openai;
+
+import com.example.treeze.dto.openai.PromptDto;
+import com.example.treeze.response.Response;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public interface ChangeService {
+    public Response chatGpt(PromptDto promptDto) throws Exception;
+
+    public HttpRequest requestGptApi(PromptDto promptDto) throws Exception;
+
+    public HttpResponse responseGptApi(HttpRequest request) throws Exception;
+}
